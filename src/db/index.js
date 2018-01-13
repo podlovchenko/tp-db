811823895c5d8fd37282ffb6946d331b8f9230ce
podlovchenko <podlovchenko.vera@gmail.com>
2018-01-13 16:57:54 +0300

    docker

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,15 @@ const options = {
 
 const pgp = require('pg-promise')(options);
 
-const db = pgp('postgres://docker:docker@localhost:5432/docker');
+const connectionOptions = {
+  host: 'localhost',
+  port: 5432,
+  database: 'docker',
+  user: 'docker',
+  password: 'docker',
+  poolSize: 8,
+};
+
+const db = pgp(connectionOptions);
 
 module.exports = db;
